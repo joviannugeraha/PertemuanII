6d2af3f1472da0e05f3cb1cd1d35fa5a9d37a23f
joviannugeraha <joviandmi@gmail.com>
2022-03-18 11:07:54 +0700

    Percobaan ElvisOperator

--- src/main/kotlin/31.ElvisOperator.kt
+++ src/main/kotlin/31.ElvisOperator.kt
@@ -4,3 +4,10 @@ fun main(args: Array<String>) {
 
     println(maybeWelcome?.length ?: 0)
 }
+/**Ketika kita memiliki sebuah null referensi „r‟, kita mungkin akan berpikir jika „r‟ tidak null maka
+kita gunakan, dengan kata lain kita akan menggunakan nilai non null „x‟
+val l: Int = if (b != null) b.length else -1
+
+kode ini bisa disederhanakan dengan menggunakan operator elvis “?:” sehingga menjadi;
+
+val l = b?.length ?: -1*/
